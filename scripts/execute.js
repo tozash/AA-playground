4c3f889f1e558f03dca198e8aac17d619feb65c3
tozash <sandr0xlivecs@gmail.com>
2025-07-07 21:38:46 +0400

    rewrote test.js

--- scripts/execute.js
+++ scripts/execute.js
@@ -3,7 +3,7 @@ const { hexlify, zeroPadValue, toBeHex } = require("ethers");
 
 const ENTRYPOINT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const SALT = 1234567890;
+const SALT = 12345678;
 
 
 async function main() {
--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,21 @@
 const hre = require("hardhat");
 
-const EP_ADDR = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const ENTRYPOINT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const SALT = 12345678;
 
 async function main() {
-    const code = await hre.ethers.provider.getCode(EP_ADDR);
+  const [signer0] = await hre.ethers.getSigners();
+  const owner = signer0.address;
 
-  console.log(`Code: ${code}`);
+  const factory = await hre.ethers.getContractAt("AccountFactory2", FACTORY_ADDRESS);
+
+  const sender = await factory.createAccount(owner, SALT);
+  const senderpredicted = await factory.getAddress(owner, SALT);
+
+  //assert.equal(sender, senderpredicted);
+
+  console.log(`Sender: ${sender} Predicted: ${senderpredicted}`);
 }
 
 main().catch((error) => {
